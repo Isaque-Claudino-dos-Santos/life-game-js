554dab5a5dbff75c66642bb93ac2fd40518e53d7
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-11-02 18:25:56 -0300

    get canvas element and context

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 //@ts-check
 'use strict';
+
+/**@type {HTMLCanvasElement} */
+const canvas =
+  document.querySelector('#main') ?? document.createElement('canvas');
+const context = canvas.getContext('2d');
