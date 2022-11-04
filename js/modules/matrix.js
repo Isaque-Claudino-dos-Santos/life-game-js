47eb5c20f775cd67b902d88b0c195f869cacb85e
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-11-04 19:25:53 -0300

    add current index in forEach

--- js/modules/matrix.js
+++ js/modules/matrix.js
@@ -6,9 +6,11 @@ const matrix = {};
  * @param { ({row,column}:{ row: number, column: number },[maxRow,maxColumn]:[maxRow:number,maxColumn: number]) => void} callback
  */
 matrix.forEach = ([maxRow, maxColumn], callback) => {
+  let index = 0;
   for (let i = 0; i < maxRow; i++) {
     for (let j = 0; j < maxColumn; j++) {
-      callback({ row: i, column: j }, [maxRow, maxColumn]);
+      callback({ row: i, column: j }, [maxRow, maxColumn], index);
+      index++;
     }
   }
 };
