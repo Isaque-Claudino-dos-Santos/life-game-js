f50c463b5158db7bd6f5113bd6549727c48d4eb2
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-11-05 18:19:56 -0300

    ajust code initial

--- js/index.js
+++ js/index.js
@@ -1,17 +1,23 @@
 //@ts-check
 ('use strict');
 
-import LifeGame from './src/LifeGame.js';
 import canvas2d from './modules/canvas2d.js';
-
+import LifeGame from './src/LifeGame.js';
 import UpdateLifeGame from './src/UpdateLifeGame.js';
 import RenderLifeGame from './src/RenderLifeGame.js';
+import IntervalHendler from './src/IntervalHandler.js';
 
 const canvas = canvas2d.getElement('#main');
 const context = canvas2d.getContext(canvas);
 
+//Set up Life Game
 const updateLifeGame = new UpdateLifeGame(canvas.width, 3);
 const renderLifeGame = new RenderLifeGame();
 const lifeGame = new LifeGame(context, updateLifeGame, renderLifeGame);
 
-lifeGame.next();
+//Set up interval loop Life Game
+const loopNextState = new IntervalHendler(
+  'Life Game Loop',
+  () => lifeGame.next(),
+  100
+);
